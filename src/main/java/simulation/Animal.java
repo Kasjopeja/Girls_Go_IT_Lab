e3341a4baaa824eb2cdf3e25f0fc20fa890bcc03
Aleksandra Zakręcka <alexandra2311@interia.pl>
2021-11-23 19:29:00 +0100

    Lab 10.0

--- src/main/java/simulation/Animal.java
+++ src/main/java/simulation/Animal.java
@@ -1,16 +1,54 @@
 package simulation;
 
-public class Animal {
+import java.util.Random;
+
+public class Animal implements Comparable <Animal>  {
 	private Vector2D position;
 
 	private int energy;
-	private int age;
+	private int age = 1;
+	private final Genome genome;
+	private  int numberOfChildren = 0;
+	private final int animalID;
+	private static int counter = 0;
 
 	public Animal(Vector2D position, int energy)
 	{
 		this.position = position;
 		this.energy = energy;
-		this.age = 1;
+		this.animalID = counter++;
+		this.genome = new Genome();
+	}
+
+	public Animal(Animal mother, Animal father)
+	{
+		Vector2D move = MapDirection.values()[new Random().nextInt(MapDirection.values().length)].getUnitVector();
+		this.position = pbc(mother.getPosition().add(move));
+		this.energy = (mother.getEnergy() + father.getEnergy()) / 4;
+		this.animalID = counter++;
+		this.genome = new Genome(mother.getGenome(), father.getGenome());
+		mother.setEnergy(mother.getEnergy() * 3/4);
+		father.setEnergy(father.getEnergy() * 3/4);
+		mother.increaseNumberOfChildren();
+		father.increaseNumberOfChildren();
+	}
+
+	public int getNumberOfChildren()
+	{
+		return numberOfChildren;
+	}
+
+	public void increaseNumberOfChildren()
+	{
+		numberOfChildren++;
+	}
+
+	public Genome getGenome() {
+		return genome;
+	}
+
+	public int getAnimalID() {
+		return animalID;
 	}
 
 	public Animal setEnergy(int energy)
@@ -41,7 +79,7 @@ public class Animal {
 	public void move(MapDirection direction, int width, int height) {
     
 		position = pbc(position.add(direction.getUnitVector()));
-		System.out.println("Animal moves " + direction + ": new position: " + position);
+		System.out.println("Animal " + animalID + " moves " + direction + ": new position: " + position + " Energy: " + energy + " Age: " + age);
 	}
 
 	private Vector2D pbc(Vector2D position) {
@@ -54,5 +92,13 @@ public class Animal {
 		if (position.getY() >= height) return position.subtract(new Vector2D(0, height));
 
 		return position;
+	}
+
+	public int compareTo(Animal animal)
+	{
+		return getEnergy() == animal.getEnergy()
+				? getAnimalID() - animal.getEnergy()
+				: getEnergy() - animal.getEnergy();
+
 	}
 }
--- src/main/java/simulation/IWorldMap.java
+++ src/main/java/simulation/IWorldMap.java
@@ -4,4 +4,7 @@ public interface IWorldMap {
     int getWidth();
     int getHeight();
     void run();
+    void eat();
+    void atTheEndOfDay();
+    void reproduce();
 }
